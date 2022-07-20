08f3054f5db256f09d1446efb40800131256d849
Chris Miles <akkadius1@gmail.com>
2022-07-20 01:46:52 -0500

    Consolidate command files

--- command/handle.go
+++ command/handle.go
@@ -1,13 +1,33 @@
 package command
 
-import "os"
+import (
+	"fmt"
+	"github.com/Akkadius/rclone-multi/rclone"
+	"os"
+)
 
 func Run(args []string) {
 	command := os.Args[1]
 	if command == "upload" {
-		handleUpload(os.Args)
+		if len(args) < 3 {
+			fmt.Println("Usage: upload [source-file] [destination-path]")
+			os.Exit(0)
+		}
+
+		sourceFile := args[2]
+		destPath := ""
+		if len(args) > 3 {
+			destPath = args[3]
+		}
+
+		rclone.Upload(sourceFile, destPath)
 	}
 	if command == "trim" {
-		handleTrim(os.Args)
+		if len(args) < 3 {
+			fmt.Println("Usage: trim [after-days] Example: trim 10 would trim anything older than 10 days")
+			os.Exit(0)
+		}
+
+		rclone.Trim(args[2])
 	}
 }
